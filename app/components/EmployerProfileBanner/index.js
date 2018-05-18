a9aca2be483cf80ecf4f7dfe22c11140e273c50d
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-18 23:26:01 +0700

    Update read more

--- app/components/EmployerProfileBanner/index.js
+++ app/components/EmployerProfileBanner/index.js
@@ -15,7 +15,22 @@ import WhiteEmailIcon from 'components/Icons/WhiteEmail/Loadable';
 import WebIcon from 'components/Icons/Web/Loadable';
 import AddressIcon from 'components/Icons/Address/Loadable';
 export default class EmployerProfileBanner extends PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor() {
+    super();
+    this.state = {
+      showReadMore: false,
+    };
+  }
+  handleReadMore() {
+    this.setState({
+      showReadMore: !this.state.showReadMore,
+    });
+    return false;
+  }
   render() {
+    const {
+      showReadMore,
+    } = this.state;
     return (
       <div className="EmployerProfileBanner">
         <div className="EmployerProfileBanner-container">
@@ -57,17 +72,33 @@ export default class EmployerProfileBanner extends PureComponent { // eslint-dis
               Company Description
             </div>
             <div className="EmployerProfileBanner-companyDescriptionText">
-              Tapway is growing fast as a start­up and is looking to expand our marketing and sales team. Thus, we are looking for a marketing trainee to:… <a className="EmployerProfileBanner-hrefText" href="">Read more</a>
-              Tapway is growing fast as a start­up and is looking to expand our marketing and sales team. Thus, we are looking for a marketing trainee to:
-              ● Develop marketing campaigns to promote our products & services
-              ● Communicate with target audiences and manage customer relationships
-              ● Source advertising opportunities in the press ­ local, regional, national, specialist and digital publications­ or on
-              ● Manage the production of marketing materials, including videos, leaflets, posters, flyers and newsletters
-              ● Organize events such as workshops, conferences and seminars
-              ● Manage our social media websites such as Facebook, Twitter, LinkedIn and YouTube page
-              ● Have fun in a highly dynamic team and drive innovations to continue as a leader in one of the fastest ­growing
-              Having initially focused on classic slim fit men’s shirt, the range evolved to a complete modern and contemporary motive featuring from geometrical to floral theme. Mixing the classical and out of the norm colour, makes our batik shirt the most sought after in domestic market.
-              The idea was started when two young entrepreneurs, also a good friend who appreciate the art of batik trying to cultivate, preserve and nurture our batik tradition back to a modern and hectic lifestyle. As both of them travel extensively throughout Nusantara, more and more ideas come to reality and its where Kapten Batik was born.
+              Tapway is growing fast as a start­up and is looking to expand our marketing and sales team. Thus, we are looking for a marketing trainee to:… { !showReadMore && <button onClick={() => this.handleReadMore()} className="EmployerProfileBanner-hrefText">Read more</button> }
+              { showReadMore ?
+                <span>
+                  <p>
+                    ● Develop marketing campaigns to promote our products & services
+                  </p>
+                  <p>
+                    ● Communicate with target audiences and manage customer relationships
+                  </p>
+                  <p>
+                    ● Source advertising opportunities in the press ­ local, regional, national, specialist and digital publications­ or on
+                  </p>
+                  <p>
+                    ● Manage the production of marketing materials, including videos, leaflets, posters, flyers and newsletters
+                  </p>
+                  <p>
+                    ● Organize events such as workshops, conferences and seminars
+                  </p>
+                  <p>
+                    ● Manage our social media websites such as Facebook, Twitter, LinkedIn and YouTube page
+                  </p>
+                  <p>
+                    ● Have fun in a highly dynamic team and drive innovations to continue as a leader in one of the fastest ­growing
+                  </p>
+                </span>
+                : null
+              }
             </div>
           </div>
           <div className="EmployerProfileBanner-editButton">
