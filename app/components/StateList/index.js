e67e883110c019e38307f9b2dec98cc04016bc88
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-18 17:37:08 +0700

    Update View more

--- app/components/StateList/index.js
+++ app/components/StateList/index.js
@@ -16,19 +16,81 @@ import SelangorIcon from 'components/Icons/State/Selangor/Loadable';
 import JohorIcon from 'components/Icons/State/Johor/Loadable';
 import MelakaIcon from 'components/Icons/State/Melaka/Loadable';
 import NegeriIcon from 'components/Icons/State/Negeri/Loadable';
+import ShowMoreIcon from 'components/Icons/ShowMore/Loadable';
 export default class StateList extends PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor() {
+    super();
+    this.state = {
+      limit: [
+        {
+          text: 'Kuala Lumpur',
+          iconType: <KualaLumpurIcon />,
+        },
+        {
+          text: 'Selangor',
+          iconType: <JohorIcon />,
+        },
+        {
+          text: 'Johor',
+          iconType: <JohorIcon />,
+        },
+      ],
+    };
+  }
+  handleShowMore() {
+    this.setState({
+      limit: [
+        {
+          text: 'Kuala Lumpur',
+          iconType: <KualaLumpurIcon />,
+        },
+        {
+          text: 'Selangor',
+          iconType: <SelangorIcon />,
+        },
+        {
+          text: 'Johor',
+          iconType: <JohorIcon />,
+        },
+        {
+          text: 'Melaka',
+          iconType: <MelakaIcon />,
+        },
+        {
+          text: 'Negeri',
+          iconType: <NegeriIcon />,
+        },
+      ],
+    });
+  }
   render() {
+    const {
+      limit,
+    } = this.state;
+    const listItem = [];
+    limit.map((item) => listItem.push(<CategoryItem text={item.text} iconType={item.iconType} />));
     return (
       <div>
         <div className="StateList-title">
           BROWSE STATES
         </div>
         <div className="StateList">
-          <CategoryItem text="Kuala Lumpur" iconType={<KualaLumpurIcon />} />
+          { listItem }
+          {/* <CategoryItem text="Kuala Lumpur" iconType={<KualaLumpurIcon />} />
           <CategoryItem text="Selangor" iconType={<SelangorIcon />} />
           <CategoryItem text="Johor" iconType={<JohorIcon />} />
           <CategoryItem text="Melaka" iconType={<MelakaIcon />} />
-          <CategoryItem text="Negeri Sembilan" iconType={<NegeriIcon />} />
+          <CategoryItem text="Negeri Sembilan" iconType={<NegeriIcon />} /> */}
+          <button className="StateList-button" onClick={() => this.handleShowMore()}>
+            <div className="StateList-buttonContainer">
+              <div className="StateList-showMoreIcon">
+                <ShowMoreIcon />
+              </div>
+              <div className="StateList-buttonText">
+                VIEW MORE...
+              </div>
+            </div>
+          </button>
         </div>
       </div>
     );
