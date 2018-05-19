48a358144041649d1843ea65badb864bddffaa5e
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-19 09:57:25 +0700

    Update URl

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
   NAME: 'N.exchange',
   DOMAIN: 'https://n.exchange',
-  BASE_URL: 'http://localhost:3001',
-  // BASE_URL: 'https://parttimebee.my',
+  // BASE_URL: 'http://localhost:3001',
+  BASE_URL: 'https://parttimebee.my',
   SUPPORT_EMAIL: 'support@nexchange.io',
   PRICE_FETCH_INTERVAL: 60000,
   ORDER_DETAILS_FETCH_INTERVAL: 20000,
