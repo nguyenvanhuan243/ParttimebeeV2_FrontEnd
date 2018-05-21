3528d5c36989313f37214e287f4b2ab9058f436b
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-21 16:35:34 +0700

    Update StateList

--- app/components/StateList/index.js
+++ app/components/StateList/index.js
@@ -10,13 +10,24 @@
  */
 
 import React, { PureComponent } from 'react';
+import ShowMoreIcon from 'components/Icons/ShowMore/Loadable';
 import CategoryItem from 'components/CategoryList/CategoryItem/Loadable';
 import KualaLumpurIcon from 'components/Icons/State/KualaLumpur/Loadable';
-import SelangorIcon from 'components/Icons/State/Selangor/Loadable';
 import JohorIcon from 'components/Icons/State/Johor/Loadable';
 import MelakaIcon from 'components/Icons/State/Melaka/Loadable';
 import NegeriIcon from 'components/Icons/State/Negeri/Loadable';
-import ShowMoreIcon from 'components/Icons/ShowMore/Loadable';
+import SelangorIcon from 'components/Icons/State/Selangor/Loadable';
+import SarawakIcon from 'components/Icons/State/Sarawak/Loadable';
+import PutrajayaIcon from 'components/Icons/State/Putrajaya/Loadable';
+import KedahIcon from 'components/Icons/State/Kedah/Loadable';
+import KelantanIcon from 'components/Icons/State/Kelantan/Loadable';
+import PahangIcon from 'components/Icons/State/Pahang/Loadable';
+import LabuanIcon from 'components/Icons/State/Labuan/Loadable';
+import PerakIcon from 'components/Icons/State/Perak/Loadable';
+import PerlisIcon from 'components/Icons/State/Perlis/Loadable';
+import PenangIcon from 'components/Icons/State/Penang/Loadable';
+import TerengganuIcon from 'components/Icons/State/Terengganu/Loadable';
+import SabahIcon from 'components/Icons/State/Sabah/Loadable';
 export default class StateList extends PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor() {
     super();
@@ -33,6 +44,14 @@ export default class StateList extends PureComponent { // eslint-disable-line re
         {
           text: 'Johor',
           iconType: <JohorIcon />,
+        },
+        {
+          text: 'Melaka',
+          iconType: <MelakaIcon />,
+        },
+        {
+          text: 'Negeri',
+          iconType: <NegeriIcon />,
         },
       ],
     };
@@ -59,6 +78,50 @@ export default class StateList extends PureComponent { // eslint-disable-line re
         {
           text: 'Negeri',
           iconType: <NegeriIcon />,
+        },
+        {
+          text: 'Putrajaya',
+          iconType: <PutrajayaIcon />,
+        },
+        {
+          text: 'Kedah',
+          iconType: <KedahIcon />,
+        },
+        {
+          text: 'Kelantan',
+          iconType: <KelantanIcon />,
+        },
+        {
+          text: 'Pahang',
+          iconType: <PahangIcon />,
+        },
+        {
+          text: 'Labuan',
+          iconType: <LabuanIcon />,
+        },
+        {
+          text: 'Perak',
+          iconType: <PerakIcon />,
+        },
+        {
+          text: 'Perlis',
+          iconType: <PerlisIcon />,
+        },
+        {
+          text: 'Penang',
+          iconType: <PenangIcon />,
+        },
+        {
+          text: 'Terengganu',
+          iconType: <TerengganuIcon />,
+        },
+        {
+          text: 'Sabah',
+          iconType: <SabahIcon />,
+        },
+        {
+          text: 'Sarawak',
+          iconType: <SarawakIcon />,
         },
       ],
     });
