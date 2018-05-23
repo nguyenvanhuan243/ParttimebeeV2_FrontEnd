80911e3f004fff11beecc000f8b94aa78897e5cc
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-23 16:21:39 +0700

    Update JobList

--- app/components/JobList/JobItem/index.js
+++ app/components/JobList/JobItem/index.js
@@ -25,68 +25,70 @@ export default class JobItem extends PureComponent { // eslint-disable-line reac
       showDelete = false,
     } = this.props;
     return (
-      <a className="JobItem-href" href={`${config.BASE_URL}/job-detail`}>
-        <div className="JobItem">
+      <div className="JobItem">
+        <a className="JobItem-href" href={`${config.BASE_URL}/job-detail`}>
           <div className="JobItem-image">
           </div>
-          <div className="JobItem-content">
-            <div className="JobItem-contentTitle">
+        </a>
+        <div className="JobItem-content">
+          <div className="JobItem-contentTitle">
+            <a className="JobItem-href" href={`${config.BASE_URL}/job-detail`}>
               Presentable Chinese Female Crew - KL/Selangor
-            </div>
-            <div className="JobItem-contentCompanyName">
-              Company Name Here
-            </div>
-            <div className="JobItem-contentFooter">
-              <div className="JobItem-contentJobState">
-                <div className="JobItem-contentJobStateText">
-                  KUALALUMPUR
-                </div>
+            </a>
+          </div>
+          <div className="JobItem-contentCompanyName">
+            Company Name Here
+          </div>
+          <div className="JobItem-contentFooter">
+            <div className="JobItem-contentJobState">
+              <div className="JobItem-contentJobStateText">
+                KUALALUMPUR
               </div>
-              { showCity ?
-                <div className="JobItem-contentJobCity">
-                  <div className="JobItem-contentJobCityText">
-                    PUTRAJAYA
-                  </div>
-                </div> : null }
-              { showView ?
-                <div className="JobItem-contentViewContainer">
-                  <div className="JobItem-contentViewIcon">
-                    <ViewIcon />
-                  </div>
-                  <div className="JobItem-contentViewNumber">
-                    <span>
-                      888
-                    </span>
-                  </div>
-                </div> : null }
-              { showShare ?
-                <div className="JobItem-contentShareContainer">
-                  <div className="JobItem-contentShareIcon">
-                    <ShareIcon />
-                  </div>
-                  <div className="JobItem-contentShareText">
-                    SHARE
-                  </div>
-                </div> : null }
-              { showDelete ?
-                <div className="JobItem-contentShareContainer">
-                  <div className="JobItem-contentShareIcon">
-                    <DeleteIcon />
-                  </div>
-                </div> : null }
-              { showEdit ?
-                <div className="JobItem-contentShareContainer">
-                  <div className="JobItem-contentShareIcon">
-                    <ShareIcon />
-                  </div>
-                  <div className="JobItem-contentShareText">
-                    EDIT
-                  </div>
-                </div> : null }
             </div>
+            { showCity ?
+              <div className="JobItem-contentJobCity">
+                <div className="JobItem-contentJobCityText">
+                  PUTRAJAYA
+                </div>
+              </div> : null }
+            { showView ?
+              <div className="JobItem-contentViewContainer">
+                <div className="JobItem-contentViewIcon">
+                  <ViewIcon />
+                </div>
+                <div className="JobItem-contentViewNumber">
+                  <span>
+                    888
+                  </span>
+                </div>
+              </div> : null }
+            { showShare ?
+              <div className="JobItem-contentShareContainer">
+                <div className="JobItem-contentShareIcon">
+                  <ShareIcon />
+                </div>
+                <div className="JobItem-contentShareText">
+                  SHARE
+                </div>
+              </div> : null }
+            { showDelete ?
+              <div className="JobItem-contentShareContainer">
+                <div className="JobItem-contentShareIcon">
+                  <DeleteIcon />
+                </div>
+              </div> : null }
+            { showEdit ?
+              <div className="JobItem-contentShareContainer">
+                <div className="JobItem-contentShareIcon">
+                  <ShareIcon />
+                </div>
+                <div className="JobItem-contentShareText">
+                  EDIT
+                </div>
+              </div> : null }
           </div>
         </div>
-      </a>
+      </div>
     );
   }
 }
