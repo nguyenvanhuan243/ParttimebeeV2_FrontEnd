df32cbc52d7ab7d0e837094588c38e7dca7781c1
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-28 16:17:07 +0700

    Update sign up feature

--- app/components/LoginRegister/Signup/index.js
+++ app/components/LoginRegister/Signup/index.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
+import axios from 'axios';
 import DashlineIcon from 'components/LoginRegister/GeneralComponent/DashlineIcon/Loadable';
-import FormComponent from 'components/LoginRegister/GeneralComponent/Form/Loadable';
+import ShowPasswordIcon from 'components/LoginRegister/GeneralComponent/ShowPasswordIcon/Loadable';
+import { Alert } from 'reactstrap';
 import config from '../../../../config';
 
 export default class Signup extends PureComponent { // eslint-disable-line react/prefer-stateless-function
@@ -8,7 +10,9 @@ export default class Signup extends PureComponent { // eslint-disable-line react
     super();
     this.state = {
       isLoading: true,
+      isSubmited: false,
     };
+    this.onSubmit = this.onSubmit.bind(this);
   }
   // After 0.1s then Signup component will be rendered.
   componentWillMount() {
@@ -21,10 +25,23 @@ export default class Signup extends PureComponent { // eslint-disable-line react
       100
     );
   }
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      isSubmited: !this.state.isSubmited,
+    });
+    axios.post(`${config.API_BASE_URL}/users?email=${this.email.value}&password=${this.password.value}`);
+  }
   render() {
-    if (this.state.isLoading) {
+    const {
+      isSubmited,
+      isLoading,
+    } = this.state;
+
+    if (isLoading) {
       return null;
     }
+
     return (
       <div className="Signup">
         <DashlineIcon />
@@ -35,7 +52,44 @@ export default class Signup extends PureComponent { // eslint-disable-line react
           It’s Free!
         </div>
         <div className="Signup-form">
-          <FormComponent />
+          <div className="FormComponent">
+            {
+              isSubmited ?
+                <Alert color="success">
+                  Your account has been registered, Please check email to confirm it.
+                </Alert> : null
+            }
+
+            <form onSubmit={this.onSubmit}>
+              <div className="FormComponent-inputCustom">
+                <input
+                  className="FormComponent-inputHoverEmail"
+                  type="text"
+                  placeholder="Email"
+                  ref={(ref) => (this.email = ref)}
+                />
+                <div className="FormComponent-separate" />
+                <div className="FormComponent-placeHolderTop">
+                  Password
+                </div>
+                <div className="FormComponent-passwordContainer">
+                  <input
+                    className="FormComponent-inputHover"
+                    type="password"
+                    placeholder="Password"
+                    ref={(ref) => (this.password = ref)}
+                  />
+                  <ShowPasswordIcon />
+                </div>
+                <div className="FormComponent-separateColor" />
+                <button className="FormComponent-button">
+                  <div className="FormComponent-buttonText">
+                    Sign up FREE
+                  </div>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
         <div className="Signup-loginHere">
           Aready have an account?
--- config.js
+++ config.js
@@ -3,14 +3,8 @@ const config = {
   DOMAIN: 'https://n.exchange',
   // BASE_URL: 'http://localhost:3001/test',
   BASE_URL: 'https://parttimebee.my/test',
-  SUPPORT_EMAIL: 'support@nexchange.io',
-  PRICE_FETCH_INTERVAL: 60000,
-  ORDER_DETAILS_FETCH_INTERVAL: 20000,
-  RECENT_ORDERS_INTERVAL: 20000,
-  RECENT_ORDERS_COUNT: 11,
-  PRICE_COMPARISON_INTERVAL: 60000,
-  REFERRAL_CODE: null,
-  KYC_DETAILS_FETCH_INTERVAL: 20000,
+  // API_BASE_URL: 'http://localhost:3000/api/v1',
+  API_BASE_URL: 'https://api.parttimebee.com/api/v1',
 };
 
 export default config;
