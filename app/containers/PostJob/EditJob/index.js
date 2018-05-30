78423d0577d3ba3c302a79b5abf42cc4d170ae69
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-30 16:18:43 +0700

    Update Edit job

--- app/containers/PostJob/EditJob/index.js
+++ app/containers/PostJob/EditJob/index.js
@@ -11,6 +11,7 @@
 
 import React, { Component } from 'react';
 import Header from 'components/Header/Loadable';
+
 export default class EditJob extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
@@ -22,14 +23,63 @@ export default class EditJob extends Component { // eslint-disable-line react/pr
           </div>
           <div className="EditJob-content">
             <div className="EditJob-form">
+              <div className="EditJob-formContainer">
+                <div className="EditJob-lableContainer">
+                  <div className="EditJob-lableTitle">
+                    Title
+                  </div>
+                  <div className="EditJob-lableCategory">
+                    Job Category
+                  </div>
+                  <div className="EditJob-lableDescription">
+                    Job Description
+                  </div>
+                  <div className="EditJob-lableSalary">
+                    Salary
+                  </div>
+                  <div className="EditJob-lableSalaryType">
+                    Salary type
+                  </div>
+                  <div className="EditJob-lableSalaryState">
+                    Salary state
+                  </div>
+                  <div className="EditJob-lableSalaryState">
+                    City (optional)
+                  </div>
+                </div>
+                <div className="EditJob-inputContainer">
+                  <form>
+                    <input />
+                    <div className="EditJob-separateActive"></div>
+                    <select className="EditJob-inputCategory">
+                      <option>
+                        Category 1
+                      </option>
+                      <option>
+                        Category 2
+                      </option>
+                      <option>
+                        Category 3
+                      </option>
+                      <option>
+                        Category 4
+                      </option>
+                    </select>
+                  </form>
+                </div>
+              </div>
             </div>
             <div className="EditJob-sideBar">
               <div className="EditJob-sideBarTitle">
                 Title
               </div>
               <div className="EditJob-sideBarAbstract">
-                Title of the job. E.g.
-                Medical Sales Executive
+                <div>
+                  Title of the job. E.g.
+                </div>
+                <div>
+                  Medical Sales Executive
+                </div>
               </div>
             </div>
           </div>
