16db90498db15e3bfa2864727654bcdc82ef3884
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-31 20:16:16 +0700

    Update Home page

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,9 +5,38 @@ import JobList from 'components/JobList/Loadable';
 import Subscribe from 'components/Subscribe/Loadable';
 import Sponsored from 'components/Sponsored/Loadable';
 import CategoryList from 'components/CategoryList/Loadable';
+import LoadingJobsList from 'components/LoadingJobs/LoadingJobsList/Loadable';
 
 export default class HomePage extends PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    const pageHeight = document.documentElement.offsetHeight;
+    const windowHeight = window.innerHeight;
+    const scrollPosition = window.scrollY || window.pageYOffset || document.body.scrollTop + (document.documentElement && (document.documentElement.scrollTop || 0));
+    window.addEventListener('scroll', () => {
+      if (pageHeight === windowHeight + scrollPosition) {
+        setTimeout(
+          () => this.setState({
+            isLoading: true,
+          }), 3000);
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', () => this.handleScroll());
+  }
+
   render() {
+    const {
+      isLoading,
+    } = this.state;
     return (
       <div>
         <Header />
@@ -17,6 +46,10 @@ export default class HomePage extends PureComponent { // eslint-disable-line rea
           </div>
           <div className="HomePageContainer-jobListContainer">
             <JobList />
+            <JobList />
+            <JobList />
+            <JobList />
+            { isLoading ? null : <LoadingJobsList /> }
           </div>
           <div className="HomePageContainer-sidebarContainer">
             <Subscribe />
