2932237b57984bd87a8ab1a81e1e93e7b46b286c
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-01 07:16:18 +0700

    Update sponsored

--- app/components/Sponsored/index.js
+++ app/components/Sponsored/index.js
@@ -1,28 +1,9 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
+import React from 'react';
 
-import React, { PureComponent } from 'react';
-
-export default class Sponsored extends PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div className="Sponsored">
-        <div className="Sponsored-container">
-          <div className="Sponsored-title">
-            Sponsored
-          </div>
-          <div className="Sponsored-image">
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const Sponsored = () => (
+  <div className="Sponsored">
+    <div className="Sponsored-title">Sponsored</div>
+    <div className="Sponsored-image" />
+  </div>
+);
+export default Sponsored;
