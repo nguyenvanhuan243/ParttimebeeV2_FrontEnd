668677a4e89ceb050f497d031b7a43a48ddcd6b7
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-01 16:33:10 +0700

    Refactore parttimebee

--- app/components/Pages/About/index.js
+++ app/components/Pages/About/index.js
@@ -1,50 +1,26 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
-import React, { PureComponent } from 'react';
+import React from 'react';
 import Header from 'components/Header/Loadable';
 import DashlineIcon from 'components/LoginRegister/GeneralComponent/DashlineIcon/Loadable';
-export default class AboutPage extends PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="AboutPage-container">
-          <div className="AboutPage-titleContainer">
-            <div className="AboutPage-title">
-              Big bold text here
-            </div>
-            <div className="AboutPage-abstract">
-              A toolkit for makers to ship awesome products
-            </div>
-          </div>
-          <div className="AboutPage-dashlineIcon">
-            <DashlineIcon showLogo={false} />
-          </div>
-          <div className="AboutPage-content">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            Why do we use it?
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.
-          </div>
-          <div className="AboutPage-footer">
-            <div className="AboutPage-devider">
-            </div>
-            <div className="AboutPage-copyRight">
-              Copyright © 2018 - ParttimeBee
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
 
+const AboutPage = () => (
+  <div>
+    <Header />
+    <div className="AboutPage-container">
+      <div className="AboutPage-titleContainer">
+        <div className="AboutPage-title"> Big bold text here </div>
+        <div className="AboutPage-abstract"> A toolkit for makers to ship awesome products </div>
+      </div>
+      <div className="AboutPage-dashlineIcon"> <DashlineIcon showLogo={false} /> </div>
+      <div className="AboutPage-content">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        Why do we use it?
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.
+      </div>
+      <div className="AboutPage-footer">
+        <div className="AboutPage-devider" />
+        <div className="AboutPage-copyRight"> Copyright © 2018 - ParttimeBee </div>
+      </div>
+    </div>
+  </div>);
+export default AboutPage;
--- app/components/Pages/Terms/index.js
+++ app/components/Pages/Terms/index.js
@@ -1,50 +1,31 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
-import React, { PureComponent } from 'react';
+import React from 'react';
 import Header from 'components/Header/Loadable';
-export default class TermsPage extends PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="TermsPage-container">
-          <div className="TermsPage-termsOfService">
-            Terms of Service
+const TermsPage = () => (
+  <div>
+    <Header />
+    <div className="TermsPage-container">
+      <div className="TermsPage-termsOfService"> Terms of Service </div>
+      <div className="TermsPage-contentContainer">
+        <div className="TermsPage-sectionContainer">
+          <div className="TermsPage-section1">
+            PLEASE NOTE THAT YOUR USE OF AND ACCESS TO OUR SERVICES (DEFINED BELOW) ARE SUBJECT TO THE FOLLOWING TERMS; IF YOU DO NOT AGREE TO ALL OF THE FOLLOWING, YOU MAY NOT USE OR ACCESS THE SERVICES IN ANY MANNER.!
           </div>
-          <div className="TermsPage-contentContainer">
-            <div className="TermsPage-sectionContainer">
-              <div className="TermsPage-section1">
-                PLEASE NOTE THAT YOUR USE OF AND ACCESS TO OUR SERVICES (DEFINED BELOW) ARE SUBJECT TO THE FOLLOWING TERMS; IF YOU DO NOT AGREE TO ALL OF THE FOLLOWING, YOU MAY NOT USE OR ACCESS THE SERVICES IN ANY MANNER.!
-              </div>
-              <div className="TermsPage-section2">
-                Effective date: August 17th 2014 <br></br>
-                <br></br>
-                Welcome to Product Hunt. Please read on to learn the rules and restrictions that govern your use of our website(s), products, services and applications (the “Services”). If you have any questions, comments, or concerns regarding these terms or the Services, please contact us at hello@producthunt.com or 90 Gold St, FLR 3, San Francisco, CA 94133.
-                <br></br><br></br>
-                These Terms of Use (the “Terms”) are a binding contract between you and PRODUCT HUNT, INC. (“Product Hunt,” “we” and “us”). You must agree to and accept all of the Terms, or you don’t have the right to use the Services. Your using the Services in any way means that you agree to all of these Terms, and these Terms will remain in effect while you use the Services. These Terms include the provisions in this document, as well as those in the  Privacy Policy and  Copyright Dispute Policy.
-              </div>
-              <div className="TermsPage-section3">
-                Will these Terms ever change?
-              </div>
-              <div className="TermsPage-section4">
-                We are constantly trying to improve our Services, so these Terms may need to change along with the Services. We reserve the right to change the Terms at any<br></br> time, but if we do, we will bring it to your attention by placing a notice on the producthunt.com website, by sending you an email, and/or by some other means.<br></br><br></br>
-                If you don’t agree with the new Terms, you are free to reject them; unfortunately, that means you will no longer be able to use the Services. If you use the <br></br> Services in any way after a change to the Terms is effective, that means you agree to all of the changes.<br></br><br></br>
-                Except for changes by us as described here, no other amendment or modification of these Terms will be effective unless in writing and signed by both you and us.
-              </div>
-            </div>
+          <div className="TermsPage-section2">
+            Effective date: August 17th 2014 <br></br>
+            <br></br>
+            Welcome to Product Hunt. Please read on to learn the rules and restrictions that govern your use of our website(s), products, services and applications (the “Services”). If you have any questions, comments, or concerns regarding these terms or the Services, please contact us at hello@producthunt.com or 90 Gold St, FLR 3, San Francisco, CA 94133.
+            <br></br><br></br>
+            These Terms of Use (the “Terms”) are a binding contract between you and PRODUCT HUNT, INC. (“Product Hunt,” “we” and “us”). You must agree to and accept all of the Terms, or you don’t have the right to use the Services. Your using the Services in any way means that you agree to all of these Terms, and these Terms will remain in effect while you use the Services. These Terms include the provisions in this document, as well as those in the  Privacy Policy and  Copyright Dispute Policy.
+          </div>
+          <div className="TermsPage-section3"> Will these Terms ever change? </div>
+          <div className="TermsPage-section4">
+            We are constantly trying to improve our Services, so these Terms may need to change along with the Services. We reserve the right to change the Terms at any<br></br> time, but if we do, we will bring it to your attention by placing a notice on the producthunt.com website, by sending you an email, and/or by some other means.<br></br><br></br>
+            If you don’t agree with the new Terms, you are free to reject them; unfortunately, that means you will no longer be able to use the Services. If you use the <br></br> Services in any way after a change to the Terms is effective, that means you agree to all of the changes.<br></br><br></br>
+            Except for changes by us as described here, no other amendment or modification of these Terms will be effective unless in writing and signed by both you and us.
           </div>
         </div>
       </div>
-    );
-  }
-}
-
+    </div>
+  </div>
+);
+export default TermsPage;
