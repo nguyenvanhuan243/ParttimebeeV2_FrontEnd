3c33c0ca96500cd2515be23c9756c672ae9c96f6
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-04 16:36:40 +0700

    Refactor code

--- app/components/JobList/JobItem/index.js
+++ app/components/JobList/JobItem/index.js
@@ -7,78 +7,45 @@ import config from '../../../../config';
 export default class JobItem extends PureComponent {
   render() {
     const {
-      showCity = true,
-      showView = true,
-      showShare = true,
-      showEdit = false,
-      showDelete = false,
-      showImage = true,
-      title = '',
-      href = '',
-      onClickJobItem = () => {},
+      showCity = true, showView = true, showShare = true, showEdit = false, showDelete = false,
+      showImage = true, title = '', href = '', onClickJobItem = () => {},
     } = this.props;
     return (
       <div>
         <div className="JobItem">
-          { showImage ?
-            <a className="JobItem-href" href={href}>
-              <div className="JobItem-image">
-              </div>
-            </a> : null }
+          { showImage && <a className="JobItem-href" href={href}> <div className="JobItem-image" /> </a> }
           <div className="JobItem-content">
             <div className="JobItem-contentTitle">
-              <a className="JobItem-href" href={href}>
-                { title }
-              </a>
+              <a className="JobItem-href" href={href}> { title } </a>
             </div>
-            <div className="JobItem-contentCompanyName">
-              Company Name Here
-            </div>
+            <div className="JobItem-contentCompanyName"> Company Name Here </div>
             <div className="JobItem-contentFooter">
               <div className="JobItem-contentJobState">
-                <div className="JobItem-contentJobStateText">
-                  KUALALUMPUR
-                </div>
+                <div className="JobItem-contentJobStateText"> KUALALUMPUR </div>
               </div>
-              { showCity ?
+              { showCity &&
                 <div className="JobItem-contentJobCity">
-                  <div className="JobItem-contentJobCityText">
-                    PUTRAJAYA
-                  </div>
-                </div> : null }
-              { showView ?
+                  <div className="JobItem-contentJobCityText"> PUTRAJAYA </div>
+                </div> }
+              { showView &&
                 <div className="JobItem-contentViewContainer">
-                  <div className="JobItem-contentViewIcon">
-                    <ViewIcon />
-                  </div>
-                  <div className="JobItem-contentViewNumber">
-                    <span>
-                      888
-                    </span>
-                  </div>
-                </div> : null }
-              { showShare ?
+                  <div className="JobItem-contentViewIcon"> <ViewIcon /> </div>
+                  <div className="JobItem-contentViewNumber"> <span> 888 </span> </div>
+                </div> }
+              { showShare &&
                 <div className="JobItem-contentShareContainer">
-                  <div className="JobItem-contentShareIcon">
-                    <ShareIcon />
-                  </div>
-                  <div className="JobItem-contentShareText">
-                    SHARE
-                  </div>
-                </div> : null }
-              { showDelete ?
+                  <div className="JobItem-contentShareIcon"> <ShareIcon /> </div>
+                  <div className="JobItem-contentShareText"> SHARE </div>
+                </div> }
+              { showDelete &&
                 <button onClick={onClickJobItem} className="JobItem-deleteIcon">
                   <DeleteIcon />
-                </button> : null }
-              { showEdit ?
+                </button> }
+              { showEdit &&
                 <a href={`${config.BASE_URL}/postjobs-edit-job`} className="JobItem-contentEditContainer">
-                  <div>
-                    <ShareIcon />
-                  </div>
-                  <div className="JobItem-contentEditText">
-                    EDIT
-                  </div>
-                </a> : null }
+                  <div> <ShareIcon /> </div>
+                  <div className="JobItem-contentEditText"> EDIT </div>
+                </a> }
             </div>
           </div>
         </div>
