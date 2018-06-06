85b3c3bd93f655c8792e5aaf5dcd947db71a923e
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-06 21:39:47 +0700

    add important icon

---
+++ app/components/Icons/ImportantIcon/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const ImportantIcon = () => (
+  <svg
+    version="1.1"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    enableBackground="new 0 0 24 24"
+    xmlSpace="preserve"
+  >
+    <image
+      width={24}
+      height={24}
+      x={0}
+      y={0}
+      xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+      AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAdVBMVEUAAAD/rQD/rAD/qwD/
+      qgD/qgD/sQD/qwD/qgD/qgD/qgD/qwD/qgD/qwD/qwD/rAD/tgD/sQD/rAD/qwD/qwD/qwD/qgD/
+      qwD/qgD/rQD/qgD/qgD/qwD/qgD/qgD/qgD/qwD//wD/qgD/qgD/qwD/qgD////rU4fWAAAAJXRS
+      TlMAHGu22fMam/z51rxO8sJWDg1u/uxP/dchRNjn5vDVrkMB8etbyAwDmgAAAAFiS0dEJloImLUA
+      AAAHdElNRQfiBgYAJSdwit/ZAAAA00lEQVQoz3VSWRaDIAyMAi6ggopW26rVyv2vWFR44tL8JGRC
+      lkkANvF8hAnByPfAlSCMVEwZo7GKwmD3J6nKuFisnGcqTay/KGW1R1WyLkx8+WhWo21X1Tzr9U+Q
+      ys0Pr/emG5kudUJl83SdzaZC3WeUwRmALPLAV/wKcOUDivMrIGIEuIcrAD0Gwu4ARv4DmN4BFOvi
+      wr6GwS3utLvL0q4zYPtxB9wpGadpdCjRJH4NifN8IBGS2tBuxNKuF1XfL8qsdqVMHFZrjqFnrD8d
+      A9yczw8BrQ3NdS5tDAAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAxOC0wNi0wNlQwMDozNzozOS0wNzow
+      MCbQ/9QAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMTgtMDYtMDZUMDA6Mzc6MzktMDc6MDBXjUdoAAAA
+      AElFTkSuQmCC"
+    />
+  </svg>
+);
+export default ImportantIcon;
