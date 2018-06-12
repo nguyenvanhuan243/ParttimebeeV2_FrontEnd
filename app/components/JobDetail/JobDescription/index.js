7c30c6886cfca747c9a9d5a77bd64f1fa06f63e3
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-13 02:01:55 +0700

    Update Preview job

--- app/components/JobDetail/JobDescription/index.js
+++ app/components/JobDetail/JobDescription/index.js
@@ -1,14 +1,29 @@
 import React, { PureComponent, PropTypes } from 'react';
+
 export default class JobDescription extends PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     const {
       text = '',
     } = this.props;
+    const buttonRendered = (location.search === '?preview');
     return (
       <div className="JobDescription">
         <div className="JobDescription-content">
           { text }
         </div>
+        { buttonRendered ?
+          <div>
+            <button className="JobDescription-editButton">
+              <span className="JobDescription-editButtonText">
+                EDIT JOB
+              </span>
+            </button>
+            <button className="JobDescription-postButton">
+              <span className="JobDescription-postButtonText">
+                POST JOB
+              </span>
+            </button>
+          </div> : null }
       </div>
     );
   }
