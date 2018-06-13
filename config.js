14a3872eacf620ec403aa812f563cdd227e86055
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-13 21:40:16 +0700

    Update config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
   NAME: 'N.exchange',
   DOMAIN: 'https://n.exchange',
-  BASE_URL: 'http://localhost:3001/test',
-  // BASE_URL: 'https://parttimebee.my/test',
-  API_BASE_URL: 'http://localhost:3000/api/v1',
-  // API_BASE_URL: 'https://api.parttimebee.com/api/v1',
+  // BASE_URL: 'http://localhost:3001/test',
+  BASE_URL: 'https://parttimebee.my/test',
+  // API_BASE_URL: 'http://localhost:3000/api/v1',
+  API_BASE_URL: 'https://api.parttimebee.com/api/v1',
 };
 
 export default config;
