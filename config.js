6f79b5b025f6f44075e5cdb7b76ba19e2b324e1a
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-14 03:46:53 +0700

    Update config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
   // BASE_URL: 'http://localhost:3001/test',
   BASE_URL: 'https://parttimebee.my/test',
   // API_BASE_URL: 'http://localhost:3000/api/v1',
-  API_BASE_URL: 'https://api.parttimebee.com/api/v1',
+  API_BASE_URL: 'https://api.parttimebee.my/api/v1',
 };
 
 export default config;
