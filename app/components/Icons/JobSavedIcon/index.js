e16429d0b4cd89b555826f953d4afab742897230
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-17 04:14:44 +0700

    Add JobSavedIcon

---
+++ app/components/Icons/JobSavedIcon/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const JobSavedIcon = () => (
+  <svg
+    version="1.1"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    width="21px"
+    height="21px"
+    viewBox="0 0 21 21"
+    enableBackground="new 0 0 21 21"
+    xmlSpace="preserve"
+  >
+    <image
+      width={21}
+      height={21}
+      x={0}
+      y={0}
+      xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAVCAYAAACpF6WWAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+      AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QA/wD/AP+gvaeTAAAF
+      r0lEQVQ4y42TeVCUBRjGn+/YXb5vlz0+3AvkWNB1EUEF5BAJMy9QG7FBHUcpO8wap1PNLBtmzMwc
+      LTQrpiJKrTBHyyxvWqeDAVeRkMNAQVwOd9flcvfb49v9+sOhYWya6fnvfeed3x/P87zQap9XJBbX
+      rdenbTdhjPbHZ5k6M9d+35K6puj+Zp0E/xaRCzCjw3fFm0qP5q9ZQKoLlpVTlFNy589t3WOv+TBN
+      8oTIaxWyfetWH96W/Nr6cq22RDH25ohl7pTKok3Vm6fnRQNAimUql5OY/i4p0pEJiEpOj3vGviqh
+      6IDhl0PMeJSAWrTg8RVt0cZaNz98ZpGrdZ7P7ah1Ou8Fx0L/Cnvc3cOOk7kjSqaJmx9Lnj4Tjui9
+      E0sRVMIFhS5tapDnNxh8p5s2POvePW0Ymu4Om8ER6rLKeribyXRwRtkPr24COkJjoTV37SPmEClk
+      RMVsHIzRUVECNbnhdsseKipxlV4IeXUh91/lU85ttQ7Eya8neKnnUqNHSiilZ84dzd2Y1P7x5OeW
+      HR3RhVs2DVxqvHgpabLlZc3U1/NY3bBLEhLCRHh1q6fnM/6x9G+Kf6hsIwN8p44MeUiKjY78dVHD
+      woNfJMgV8QYTN8VEU0qYLEmSZbQhaHk0+uu0ewG6BQgI8TkFD+uysnP6JIJoYscZPUHhMu0NGpaX
+      lQUAgHRc3fGH0FxXIRWddQOO1lBeTkqlTC6N7RI0MMZFwcgyIDRs4jtLW7b+mJLmAi4H/VojT5kt
+      TPoTb9uYFEWdIobbU9dtqxm1hQYAu/19XiNdJ40z5i81qSLUdISMHHT6oNBoIB8Q4QkRUMWqTdyE
+      jEO2g4NvtlS0/5460dU4u34zSTTDDcANlNHIyGCB3vtQAFBOzGc519Uu0TnDPzA4rIiU2eD3yjCk
+      J6Ce74CEIUFHcsy0PMkeh7HynbyX5myWzHVkzV0hyZRJwxaKJA1SkowS/H6eGJtmxRsTtojuD3YG
+      fSLyFn4AhnCBohioVCJYmQRhCQMHlYCTbQXhY42LO1mZROTYoDscGmoDhBsSGk4xSLoJALg4q/TF
+      xu7rRHIZv5wQsnPrD69FvLkbmUWfgpW6QRFKaJQi+gNalF3YAjUxhOVZZ+G48cfe4pd/e/XBN6MA
+      oHRcytRhfV9mXK7moYhIewQnGcSt+gXwB8wYb+mEUjEMKauCKlGGDFMTFrLVmKhpgEHqMSdxuJKR
+      y7iWZfM4VY/QP9Av+5ouz145c1ZSojAnwuuFLu4GGLUPt2xF8LqzETvNBdbkBqlRQW3UglBx6L3S
+      CXmIZwUBydNKLIvUMdF81SF78z/pJynSdL6+kiUBzwnoJL0IigpMyjoPrVbETdsLaL+wHSnak2DG
+      XQJwB4ERNzruErD3+Pr7nP4TSe2+Fl/Q33zxrcyneG8gkgCAHS+YV7IDuw5yTJCeV7gPooSHVKYE
+      ZwjDS0zHvbb14IRJ8Kqu4bpjN1qbrzp7+joqbrvIbypO3WsZ9fLnbbOeZIkwRQCgjn+c+pU6lLWq
+      4bvnMSHjJnKWfAkNNwJarwf0EYAoR/9PMaj+kBiobXAc6TEzZ7pNxXT3waxjABGyvm7ON0SGCixb
+      b+wAIJKlJYkxrFSZb4i5ihlFVXC1T0erdRcERSGgV0MYCqLuSK3329NvVzVSbxRWO8rX3xz/SBdL
+      hYu47MNyAOA0KtJH0VIAIgAQpXllG9auvb0/XlcPlVwGn28WRnpegkpmRBdfxbd2fXq2vv7avk9q
+      fDVjWjPab3G0RdZlO9O9fG9f0an9dnpirH/lrYtrELs0CC6hH4i3w9Wx0W+tVJ/76gCz/0fPYitg
+      CzxQRfGBmU1Km7TT7VIeA/ARbTSf7dCNpOc5mt6D1Hg86Go/+nvNCduBjR8NnQDwIOy/NHLeeuTp
+      wUHfIACgZAG4S9UFn1v3bq99Zebu1YBe/j9B/6m/AZyiTvNgYg4uAAAAJXRFWHRkYXRlOmNyZWF0
+      ZQAyMDE4LTA2LTE2VDA2OjMyOjI2LTA3OjAw3VhQeQAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxOC0w
+      Ni0xNlQwNjozMjoyNi0wNzowMKwF6MUAAAAASUVORK5CYII="
+    />
+  </svg>
+);
+export default JobSavedIcon;
+
--- app/components/JobSavedAlert/index.js
+++ app/components/JobSavedAlert/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import JobSavedIcon from 'components/Icons/JobSavedIcon/Loadable';
 const JobSavedAlert = () => (
   <div className="JobSavedAlert">
+    <div className="JobSavedAlert-icon">
+      <JobSavedIcon />
+    </div>
+    <div className="JobSavedAlert-text">
+      Job has been saved!
+    </div>
   </div>
 );
 export default JobSavedAlert;
