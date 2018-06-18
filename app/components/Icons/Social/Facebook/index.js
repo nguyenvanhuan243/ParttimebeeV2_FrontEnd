5cf804d2dbbba204aa0c60b3bf07cecf95865d7f
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-19 06:31:12 +0700

    Update Facebook Share

--- app/components/Icons/Social/Facebook/index.js
+++ app/components/Icons/Social/Facebook/index.js
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 
-const FacebookIcon = () => (
-  <svg
-    version="1.1"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
-    x="0px"
-    y="0px"
-    width="16px"
-    height="16px"
-    viewBox="0 0 16 16"
-    enableBackground="new 0 0 16 16"
-    xmlSpace="preserve"
-  >
-    <image
-      width={16}
-      height={16}
-      x={0}
-      y={0}
-      xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+export default class FacebookIcon extends PureComponent {
+  getImage(hover) {
+    if (hover) {
+      return (
+        `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+        AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN
+        RQfiBhIIKRy8XOiBAAAAkElEQVQoz72OsQ3CMBBFvyOIlCYNDaJiBUSRikFYhoJ9YBYKVmCAIBCK
+        g3g0Z2KMpXT85uR3T74vRaFkxxkPtMqFA5/k1s2wpgu0iIy1za0Wmgc4iYSZzaPzAzSBlWotjW14
+        qnWn7/tpLrkO2QThnvD0TUnF3r6vqZgmJZ2X6I09XP97YrTDn4SrJOmmVyy8AfOeUxxP3FzaAAAA
+        JXRFWHRkYXRlOmNyZWF0ZQAyMDE4LTA2LTE4VDA4OjQxOjI4LTA3OjAwlQ9/vwAAACV0RVh0ZGF0
+        ZTptb2RpZnkAMjAxOC0wNi0xOFQwODo0MToyOC0wNzowMORSxwMAAAAASUVORK5CYII=`
+      );
+    }
+    return (
+      `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
       AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAXVBMVEUAAABVgKo9WZo8WZk7
       Wpk7Wpg8Wpk8XZk7WZk8WZo9Wpo9XJk8WqU7Wpk+Xpw9WZo8WZk9W5hAWpo7WZk8Wpk8WphVcao9
       WplJW6RmZpk8Wpo8WZhVqqo7WZj///9ifjxoAAAAHXRSTlMABn7U+vGtN/lqWBkRfTE/nUMw5fTa
@@ -25,9 +22,36 @@ const FacebookIcon = () => (
       RiZmFllWBgRgkwUBBJ8dzOdACHACuVzcPAgBXqAAI4LLxy8AFBDkF4IJyEKBME4BEaiAKEyAUUwc
       yJUQk0S1BYlLHQEpWVlpGQgTABvaCbIqhvvxAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE4LTA1LTA3
       VDAyOjMxOjIwLTA3OjAwH55orQAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxOC0wNS0wN1QwMjozMToy
-      MC0wNzowMG7D0BEAAAAASUVORK5CYII="
-    />
-  </svg>
-);
-
-export default FacebookIcon;
+      MC0wNzowMG7D0BEAAAAASUVORK5CYII=`
+    );
+  }
+  render() {
+    const {
+      hover = false,
+    } = this.props;
+    return (
+      <svg
+        version="1.1"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        width="16px"
+        height="16px"
+        viewBox="0 0 16 16"
+        enableBackground="new 0 0 16 16"
+        xmlSpace="preserve"
+      >
+        <image
+          width={16}
+          height={16}
+          x={0}
+          y={0}
+          xlinkHref={this.getImage(hover)}
+        />
+      </svg>
+    );
+  }
+}
+FacebookIcon.propTypes = {
+  hover: PropTypes.bool,
+};
--- app/components/JobDetail/JobInformation/SocialButton/Facebook/index.js
+++ app/components/JobDetail/JobInformation/SocialButton/Facebook/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import FacebookIcon from 'components/Icons/Social/Facebook/Loadable';
+import classNames from 'classnames';
 
-const Facebook = () => (
-  <div className="Facebook">
-    <div className="Facebook-icon"> <FacebookIcon /> </div>
-    <div className="Facebook-text"> SHARE </div>
-  </div>
-);
-export default Facebook;
+export default class Facebook extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      hover: false,
+    };
+  }
+  render() {
+    const {
+      hover,
+    } = this.state;
+    const facebookClassName = classNames(
+      'Facebook', {
+        'Facebook-hover': hover,
+      }
+    );
+    const facebookTextClassName = classNames(
+      'Facebook-text', {
+        'Facebook-textHover': hover,
+      }
+    );
+    return (
+      <div
+        className={facebookClassName}
+        onMouseEnter={() => this.setState({ hover: true })}
+        onMouseLeave={() => this.setState({ hover: false })}
+      >
+        <div className="Facebook-icon">
+          <FacebookIcon hover={hover} />
+        </div>
+        <div className={facebookTextClassName}>
+          SHARE
+        </div>
+      </div>
+    );
+  }
+}
