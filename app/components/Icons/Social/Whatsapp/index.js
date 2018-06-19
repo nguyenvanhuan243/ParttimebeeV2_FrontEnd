11d2e2679b34ae89b2b53d4d6a2d71d660a5f5d9
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-19 21:42:14 +0700

    Update Whatsapp

--- app/components/Icons/Social/Whatsapp/index.js
+++ app/components/Icons/Social/Whatsapp/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 
-const WhatsappIcon = () => (
+export default class WhatsappIcon extends PureComponent {
+  render() {
+    return this.props.hover ? <WhatsappIconHover /> : <WhatsappIconNormal />;
+  }
+}
+WhatsappIcon.propTypes = {
+  hover: PropTypes.bool,
+};
+
+const WhatsappIconNormal = () => (
   <svg
     version="1.1"
     xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -32,4 +41,20 @@ const WhatsappIcon = () => (
     />
   </svg>
 );
-export default WhatsappIcon;
+
+const WhatsappIconHover = () => (
+  <svg
+    width="16px"
+    height="16px"
+    viewBox="0 0 16 16"
+    version="1.1"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    <path
+      d="M8.002 0C12.413 0 16 3.589 16 8s-3.587 8-7.998 8a7.93 7.93 0 0 1-4.401-1.322l-3.075.983.997-2.972A7.94 7.94 0 0 1 0 8c0-4.412 3.587-8 7.998-8h.004zm3.837 11.794c.183-.558.183-1.032.13-1.133-.054-.101-.201-.158-.424-.279-.224-.119-1.31-.695-1.515-.772-.2-.082-.393-.053-.545.178-.214.321-.423.647-.594.844-.133.153-.352.173-.535.091-.246-.11-.934-.37-1.782-1.18C5.916 8.914 5.47 8.13 5.34 7.897c-.13-.24-.014-.379.089-.508.112-.15.218-.255.33-.394.113-.139.175-.211.247-.375.076-.158.021-.321-.033-.442-.053-.12-.5-1.291-.683-1.766-.147-.378-.26-.393-.482-.403C4.733 4.005 4.649 4 4.554 4c-.29 0-.594.092-.777.293C3.554 4.538 3 5.108 3 6.28c0 1.17.795 2.304.902 2.462.113.158 1.55 2.596 3.784 3.59 1.746.777 2.265.704 2.662.614.58-.135 1.308-.596 1.491-1.152z"
+      fill="#FFF"
+      fillRule="evenodd"
+    />
+  </svg>
+);
+
--- app/components/JobDetail/JobInformation/SocialButton/Whatsapp/index.js
+++ app/components/JobDetail/JobInformation/SocialButton/Whatsapp/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import WhatsappIcon from 'components/Icons/Social/Whatsapp/Loadable';
+import classNames from 'classnames';
 
-const Whatsapp = () => (
-  <div className="Whatsapp">
-    <div className="Whatsapp-icon"> <WhatsappIcon /> </div>
-    <div className="Whatsapp-text"> WHATSAPP </div>
-  </div>
-);
-export default Whatsapp;
+export default class Whatsapp extends PureComponent {
+  constructor() {
+    super();
+    this.state = { hover: false };
+  }
+  handleHover() {
+    this.setState({ hover: !this.state.hover });
+  }
+  render() {
+    const { hover = false } = this.state;
+    const whatsAppTextClassName = classNames('Whatsapp-text', {
+      'Whatsapp-textHover': hover,
+    });
+    const whatsAppClassName = classNames('Whatsapp', {
+      'Whatsapp-hover': hover,
+    });
+    return (
+      <div
+        className={whatsAppClassName}
+        onMouseEnter={() => this.handleHover()}
+        onMouseLeave={() => this.handleHover()}
+      >
+        <div className="Whatsapp-icon">
+          <WhatsappIcon hover={hover} />
+        </div>
+        <div className={whatsAppTextClassName}>
+          WHATSAPP
+        </div>
+      </div>
+    );
+  }
+}
