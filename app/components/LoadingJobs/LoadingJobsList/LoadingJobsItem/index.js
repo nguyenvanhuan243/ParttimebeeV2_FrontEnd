09d2f54dfa4f420a831a41db9abdf6aaeae9853a
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-22 20:34:02 +0700

    Update animation

--- app/components/LoadingJobs/LoadingJobsList/LoadingJobsItem/index.js
+++ app/components/LoadingJobs/LoadingJobsList/LoadingJobsItem/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const LoadingJobsItem = () => (
-  <div className="LoadingJobsItem animated-background">
+  <div className="LoadingJobsItem">
     <div className="LoadingJobsItem-container">
-      <div className="LoadingJobsItem-image" />
+      <div className="LoadingJobsItem-image animated-background" />
       <div className="LoadingJobsItem-rectangle">
-        <div className="LoadingJobsItem-rectangle1" />
-        <div className="LoadingJobsItem-rectangle2" />
-        <div className="LoadingJobsItem-rectangle3" />
+        <div className="LoadingJobsItem-rectangle1 animated-background" />
+        <div className="LoadingJobsItem-rectangle2 animated-background" />
+        <div className="LoadingJobsItem-rectangle3 animated-background" />
       </div>
     </div>
   </div>
