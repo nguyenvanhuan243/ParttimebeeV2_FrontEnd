b1d2faf9187b3a631e4de617a1e7c8fb85c62916
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-22 21:06:04 +0700

    Update animation

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -58,7 +58,7 @@ export default class HomePage extends PureComponent {
             scrollLoadingJob: true,
             arrayLength: this.state.limit,
           });
-        }, 1000,
+        }, 10,
       );
     }
   }
