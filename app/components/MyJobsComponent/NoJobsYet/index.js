2305a07122e7d74321030709b93eb5f8f842b915
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-07-02 23:03:02 +0700

    Update Post Now

--- app/components/MyJobsComponent/NoJobsYet/index.js
+++ app/components/MyJobsComponent/NoJobsYet/index.js
@@ -6,12 +6,13 @@ const NoJobsYet = () => (
     <div className="NoJobsYet-text">
       No jobs yet
     </div>
-    <div className="NoJobsYet-link">
-      <a className="NoJobsYet-linkHref" href={`${config.BASE_URL}/postjobs-post-job`}>
-        POST NOW
-      </a>
-    </div>
+    <a className="NoJobsYet-linkCustom" href={`${config.BASE_URL}/postjobs-post-job`}>
+      <div className="NoJobsYet-link">
+        <div className="NoJobsYet-linkHref">
+          POST NOW
+        </div>
+      </div>
+    </a>
   </div>
 );
 export default NoJobsYet;
-
