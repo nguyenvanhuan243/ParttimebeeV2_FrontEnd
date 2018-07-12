40e476a38cea93e58aa8778f02d08ffbb602cc07
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-07-12 17:52:45 +0700

    Add TickIconf

---
+++ app/components/Icons/TickIcon/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+
+export default class TickIcon extends PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="12">
+        <path fill="#C8CF2D" fillRule="evenodd" d="M15.24 2.147c.2-.158.226-.438.06-.626L14.093.16c-.166-.19-.462-.214-.66-.056L6.41 7.184 2.765 3.95c-.166-.187-.462-.212-.66-.054L.667 5.04c-.198.157-.224.437-.057.625l5.423 6.13c.167.187.462.21.66.053l8.548-9.7" />
+      </svg>
+    );
+  }
+}
+
