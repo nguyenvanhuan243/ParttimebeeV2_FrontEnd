44d305f40cad16b8009e729c0e877b743029c1ff
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-07-30 21:40:38 +0700

    Refactor code

--- app/components/CategoryList/CategoryItem/index.js
+++ app/components/CategoryList/CategoryItem/index.js
@@ -3,29 +3,11 @@ import classNames from 'classnames';
 import 'w3-css/w3.css';
 
 export default class CategoryItem extends PureComponent {
-  constructor() {
-    super();
-    this.state = { isHoverd: false };
-  }
   render() {
-    const {
-      selected,
-      onClickFunc,
-      text = 'Home',
-      iconType = 'HomeIcon',
-    } = this.props;
-    const { isHoverd } = this.state;
-    const categoryItemClassName = classNames('CategoryItem', {
-      'CategoryItem-hover': isHoverd,
-      'CategoryItem-selected': selected,
-    });
+    const { selected, onClickFunc, text = 'Home', iconType = 'HomeIcon' } = this.props;
+    const categoryItemClassName = classNames('CategoryItem', { 'CategoryItem-selected': selected });
     return (
-      <button
-        className="CategoryItem-container w3-ripple"
-        onMouseEnter={() => this.setState({ isHoverd: !this.state.isHoverd })}
-        onMouseLeave={() => this.setState({ isHoverd: !this.state.isHoverd })}
-        onClick={onClickFunc}
-      >
+      <button className="CategoryItem-container w3-ripple" onClick={onClickFunc}>
         <div className={categoryItemClassName}>
           <div className="CategoryItem-icon">
             { iconType }
