54ec88f7971f30b67d6328870494c36574e962d0
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-08-21 21:33:28 +0700

    Fix bug 22

--- app/components/JobDetail/CompanyInfo/index.js
+++ app/components/JobDetail/CompanyInfo/index.js
@@ -1,18 +1,18 @@
 import React, { PureComponent, PropTypes } from 'react';
 import ArrowIcon from 'components/Icons/Arrow/Loadable';
+import { isMac } from '../../../utils/operatingSystem';
 import config from '../../../../config';
 
 export default class CompanyInfo extends PureComponent {
   render() {
-    const {
-      userId = 0,
-    } = this.props;
+    const { userId = 0 } = this.props;
+    const fontWeight = isMac() ? '' : 'bold';
     return (
       <a className="CompanyInfo-href" href={`${config.BASE_URL}/employer-profile/${userId}`}>
         <div className="CompanyInfo">
           <div className="CompanyInfo-container">
             <div className="CompanyInfo-image" />
-            <div className="CompanyInfo-text">
+            <div style={{ fontWeight }} className="CompanyInfo-text">
               <div>View company info </div>
               <div>and all jobs by this employer </div>
             </div>
