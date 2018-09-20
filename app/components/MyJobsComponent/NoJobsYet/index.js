33da94f2a0e4d2e40a2318551362eb56937b5b6f
Hudson Nguyen <hudson.nguyen@somotsoftvn.com>
2018-09-20 16:34:26 +0700

    Update NoJobsYet

--- app/components/MyJobsComponent/NoJobsYet/index.js
+++ app/components/MyJobsComponent/NoJobsYet/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import SadIcon from 'components/Icons/Sad/Loadable';
 import config from '../../../../config';
 
+const isMyJob = location.pathname.includes('myjobs');
 const NoJobsYet = () => (
   <div className="NoJobsYet">
+    <div className="NoJobsYet-sadIcon">
+      <SadIcon />
+    </div>
     <div className="NoJobsYet-text">
       No jobs yet
     </div>
-    <a className="NoJobsYet-linkCustom" href={`${config.BASE_URL}/postjobs-post-job`}>
+    { isMyJob && <a className="NoJobsYet-linkCustom" href={`${config.BASE_URL}/postjobs-post-job`}>
       <div className="NoJobsYet-link">
         <div className="NoJobsYet-linkHref">
           POST NOW
         </div>
       </div>
-    </a>
+    </a> }
   </div>
 );
 export default NoJobsYet;
