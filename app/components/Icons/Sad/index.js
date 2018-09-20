fda40c784cec7ae1a9f93bb6f0f904656cfd91b3
Hudson Nguyen <hudson.nguyen@somotsoftvn.com>
2018-09-20 15:57:47 +0700

    Add SadIcon

---
+++ app/components/Icons/Sad/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TEXT from './constant';
+
+const SadIcon = () => (
+  <svg
+    version="1.1"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    width="18px"
+    height="18px"
+    viewBox="0 0 75 72"
+    enableBackground="new 0 0 75 72"
+    xmlSpace="preserve"
+  >
+    <image
+      width={75}
+      height={72}
+      x={0}
+      y={0}
+      xlinkHref={TEXT}
+    />
+  </svg>
+);
+
+export default SadIcon;
