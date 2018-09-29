2b3b460eaa3b440c1cc88780afcf801538734535
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-09-29 17:02:07 +0700

    add warning icon

---
+++ app/components/Icons/Warning/index.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+
+export default class WarningIcon extends PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <svg
+        version="1.1"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        width="18px"
+        height="20px"
+        viewBox="0 0 18 20"
+        enableBackground="new 0 0 18 20"
+        xmlSpace="preserve"
+      >
+        <image
+          width={18}
+          height={20}
+          x={0}
+          y={0}
+          xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAMAAAC3SZ14AAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+      AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAABNVBMVEX5+fn5+Pn5+Pj29vn9
+      3Yb923/77sz/1Sn/1if77cf+2F/82yn72in+2Fr59/b4+Pn757H/3SlvXxRtXhP/3in856v69u3+
+      00X/4yxQRQ5LQQ3/4iz+00H69ev84pT/2ij/4S1dURBWSg//4C3/2yj94Y368t7/0zP/2Cj/4Stj
+      VhBcTw//4Cv/1DH68Nj39/n92mz/2R7/1CH/4CNnVw5hUw3/2h792GT767//0hj/1Bf/0hf/3BiH
+      cAyGcAz/0xj76bf59/T+0kL/1Aj/0Ar71Ar+0Tv59vL8453/zwP/zgD/zQH+0gF7ZgF2YgH+0QH/
+      0AL84pX69OX+yiX/zQD/ygD9zwBmUwBgTgD9zgD/yyH68uD81Wr/zwD+013668D834T834H83oH8
+      3oD834P66Lf///87ImgEAAAAAWJLR0RmLNTZJQAAAAd0SU1FB+IJHQMAFs6p9hIAAAC0SURBVBjT
+      Y2AgBTBCaSa4CDMLKzOaIjZ2Dk5UESYubh5ePmQRfgFBIWERUX4kITFxCUkpaRlZJEVy8gqKSsoq
+      qggb1NQ1NLW0dTR09WAi+gaGRsYmpsZGZuYw0ywsraxtbO1srO2tHSAijk7OLi4urq5AwtnNHWy2
+      h6eXt7ePr5+/t7dXQCBIa1BwSGhoaFh4RCSQComKBgrFxIaAgBeUigMKxSckJgFBcjKITExJJSn8
+      UAEALcwiYalrKs4AAAAldEVYdGRhdGU6Y3JlYXRlADIwMTgtMDktMjlUMDM6MDA6MjItMDc6MDA3
+      wNs1AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE4LTA5LTI5VDAzOjAwOjIyLTA3OjAwRp1jiQAAAABJ
+      RU5ErkJggg=="
+        />
+      </svg>
+    );
+  }
+}
+
