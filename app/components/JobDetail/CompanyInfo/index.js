f68e6339f93e742c58d3bf1968f0db73aea4527d
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-10-13 17:38:36 +0700

    Update code

--- app/components/JobDetail/CompanyInfo/index.js
+++ app/components/JobDetail/CompanyInfo/index.js
@@ -2,10 +2,33 @@ import React, { PureComponent, PropTypes } from 'react';
 import { Link } from 'react-router-dom';
 import ArrowIcon from 'components/Icons/Arrow/Loadable';
 import RandomString from 'random-string';
+import axios from 'axios';
+import DefaultAvatar from 'components/DefaultAvatar/DefaultAvatar/Loadable';
 import { isMac } from '../../../utils/operatingSystem';
+import config from '../../../../config';
 
+const getUserFromJobIdUrl = `${config.API_BASE_URL}/users/show-user-with-job-id`;
 export default class CompanyInfo extends PureComponent {
+  constructor() {
+    super();
+    this.state = { user: {} };
+  }
+
+  componentWillMount() {
+    axios.post(getUserFromJobIdUrl, {
+      jobId: this.getJobIdFromUrl(),
+    }).then(response => {
+      this.setState({ user: response.data });
+    });
+  }
+
+  // Get job id from url
+  getJobIdFromUrl = () => (
+    location.pathname.substring(location.pathname.lastIndexOf('/') + 1)
+  );
+
   render() {
+    const { user } = this.state;
     const { userId = 0 } = this.props;
     const fontWeight = isMac() ? '' : 'bold';
     const randomeString = RandomString({ length: 50 });
@@ -13,7 +36,9 @@ export default class CompanyInfo extends PureComponent {
       <Link className="CompanyInfo-href" to={`/employer-profile/${userId}?${randomeString}`}>
         <div className="CompanyInfo">
           <div className="CompanyInfo-container">
-            <div className="CompanyInfo-image" />
+            <div className="CompanyInfo-image">
+              <DefaultAvatar defaultAvatarNumber={user.url_avatar || user.default_avatar} />
+            </div>
             <div style={{ fontWeight }} className="CompanyInfo-text">
               <div>View company info </div>
               <div>and all jobs by this employer </div>
